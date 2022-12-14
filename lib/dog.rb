1e9907275861d506fef9502d5ad48e1ad29f8d6a
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-12-14 11:00:30 -0600

    commit tree 3182bef735b8d76fde57ca309dc8a7b4cbe6ce75

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog 
+    attr_accessor :breed
+    attr_reader :name
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+    end
+end
+
+l = Dog.new("Libby")
+l.breed
+
